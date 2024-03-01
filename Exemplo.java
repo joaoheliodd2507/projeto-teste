e6fb5fbce31d1c859e1547ca8e258a41a928810e
joaoheliodd2507 <159277743+joaoheliodd2507@users.noreply.github.com>
2024-03-01 15:32:29 -0300

    primeiro commit

---
+++ Exemplo.java
@@ -0,0 +1,12 @@
+package Primeiro_projeto;
+
+public class Exemplo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("olá, mundo!");
+
+	}
+
+}
---
+++ Nome_idade.java
@@ -0,0 +1,10 @@
+package Primeiro_projeto;
+
+public class Nome_idade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
